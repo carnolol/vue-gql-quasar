a0db707bc8f72f3298a4e8ac8eda3c1c1fc3fe94
Mike Chadwick <mchadwick@blenderbottle.com>
2021-08-24 10:20:55 -0600

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" />
+
+        <q-toolbar-title> Smash Data! </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {},
+};
+</script>
---
+++ src/pages/Landing.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>HELLO WORLD</div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Landing",
+  data() {},
+};
+</script>
